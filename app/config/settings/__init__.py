9aa853e35886db68c130cefb63447e99eefb4e52
Kazzila <mrkazzila@gmail.com>
2023-05-03 19:23:53 +0300

    feat!: update settings
    Change thr settings module to thr settings package
    
    https://github.com/Kazzila/movie_website/issues/6

---
+++ app/config/settings/__init__.py
@@ -0,0 +1,2 @@
+from .common import *  # noqa: F403, F401
+from .database import DATABASES  # noqa: F401
---
+++ app/config/settings/database.py
@@ -0,0 +1,14 @@
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ['DB_ENGINE'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    },
+}
