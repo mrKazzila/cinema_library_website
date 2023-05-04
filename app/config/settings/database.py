9653eaf1cb2bebf65d4ca4b065bc3f0575e8e092
Kazzila <mrkazzila@gmail.com>
2023-05-04 12:08:50 +0300

    fix: tmp comment unusable settings fields

--- app/config/settings/database.py
+++ app/config/settings/database.py
@@ -6,9 +6,9 @@ DATABASES = {
     'default': {
         'ENGINE': environ['DB_ENGINE'],
         'NAME': environ['DB_NAME'],
-        'USER': environ['DB_USER'],
-        'PASSWORD': environ['DB_PASSWORD'],
-        'HOST': environ['DB_HOST'],
-        'PORT': environ['DB_PORT'],
+        # 'USER': environ['DB_USER'],
+        # 'PASSWORD': environ['DB_PASSWORD'],
+        # 'HOST': environ['DB_HOST'],
+        # 'PORT': environ['DB_PORT'],
     },
 }
