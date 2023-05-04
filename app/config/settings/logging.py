4827bbaa7c1217b16373f1533ec5dc3ba218d5b9
Kazzila <mrkazzila@gmail.com>
2023-05-04 11:32:04 +0300

    feat: add simple settings for logging

---
+++ app/config/settings/logging.py
@@ -0,0 +1,36 @@
+from os import getenv
+
+from common import BASE_DIR  # noqa
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'custom_formatter': {
+            'format': '[%(asctime)s] %(levelname)s %(module)s %(process:d)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+        },
+    },
+    'handlers': {
+        # 'file': {
+        #     'level': 'WARNING',
+        #     'class': 'logging.FileHandler',
+        #     'filename': BASE_DIR / 'log/log.log',
+        #     'formatter': 'custom_formatter'
+        # },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'custom_formatter',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
