ed33db7c2798bf53f0a374a15ec3850538915e3b
Kazzila <mrkazzila@gmail.com>
2023-05-04 18:34:31 +0300

    feat: update db settings

--- app/config/settings/database.py
+++ app/config/settings/database.py
@@ -1,14 +1,15 @@
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://postgresapp.com/
 from os import environ
 
 DATABASES = {
     'default': {
         'ENGINE': environ['DB_ENGINE'],
         'NAME': environ['DB_NAME'],
-        # 'USER': environ['DB_USER'],
-        # 'PASSWORD': environ['DB_PASSWORD'],
-        # 'HOST': environ['DB_HOST'],
-        # 'PORT': environ['DB_PORT'],
+        'USER': environ['DB_USER'],
+        'PASSWORD': environ['DB_PASSWORD'],
+        'HOST': environ['DB_HOST'],
+        'PORT': environ['DB_PORT'],
     },
 }
