ba1f8f3613f773ea24c9f78f4b7f3585483afa38
Kazzila <mrkazzila@gmail.com>
2023-05-05 18:48:05 +0300

    feat: add simple view

--- app/cinemalib/views.py
+++ app/cinemalib/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render  # noqa
+from django.views.generic.base import View
+from .models import Movie
 
-# Create your views here.
+
+class MovieView(View):
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request=request, template_name='cinemalib/movies.html', context={'movies_list': movies})
