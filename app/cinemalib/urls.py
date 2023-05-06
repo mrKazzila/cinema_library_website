571f4f7e8108559810ea6b119a6255d9ed82c8e3
Kazzila <mrkazzila@gmail.com>
2023-05-06 20:33:07 +0300

    feat: add new url path for detail page

--- app/cinemalib/urls.py
+++ app/cinemalib/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import MovieView
+from .views import MovieDetailView, MovieView
 
 urlpatterns = [
     path('', MovieView.as_view()),
+    path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
 ]
