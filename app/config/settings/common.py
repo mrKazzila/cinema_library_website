23b91cdb0d42aea54f44076b82ddd2d95dc74030
Kazzila <mrkazzila@gmail.com>
2023-05-07 20:20:14 +0300

    refactor: update settings for static files

--- app/config/settings/common.py
+++ app/config/settings/common.py
@@ -90,8 +90,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = BASE_DIR / '../static'
-STATICFILES_DIRS = (STATIC_DIRS,)
+if DEBUG:
+    STATICFILES_DIRS = (BASE_DIR / '../static',)
+else:
+    STATIC_ROOT = BASE_DIR / '../static'  # for prod
 
 # Media files
 
