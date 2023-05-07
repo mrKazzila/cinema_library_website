ce730311e5b34c64b59368ea0ba8e1bcce4ab0ad
Kazzila <mrkazzila@gmail.com>
2023-05-07 22:09:59 +0300

    feat: add settings for ckeditor

--- app/config/settings/__init__.py
+++ app/config/settings/__init__.py
@@ -1,3 +1,4 @@
 from .common import *  # noqa: F403, F401
 from .database import DATABASES  # noqa: F401
 from .logging import LOGGING  # noqa: F401
+from .ckeditor import CKEDITOR_CONFIGS, CKEDITOR_UPLOAD_PATH  # noqa: F401
