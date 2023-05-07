356a8dacbcd548e66c17b34922da21b0ac79edc6
Kazzila <mrkazzila@gmail.com>
2023-05-07 17:02:29 +0300

    feat: add urls for review

--- app/cinemalib/urls.py
+++ app/cinemalib/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import MovieDetailView, MovieView
+from .views import AddReviewView, MovieDetailView, MovieView
 
 urlpatterns = [
     path('', MovieView.as_view()),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
+    path('review/<int:pk>/', AddReviewView.as_view(), name='add_review'),
 ]
