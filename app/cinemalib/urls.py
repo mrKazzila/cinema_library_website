cf8a3a27c5dcbdbcb2102339d4d13498db083e21
Kazzila <mrkazzila@gmail.com>
2023-05-08 17:34:07 +0300

    feat: add url path for actors & directors

--- app/cinemalib/urls.py
+++ app/cinemalib/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AddReviewView, MovieDetailView, MovieView
+from .views import ActorView, AddReviewView, MovieDetailView, MovieView
 
 urlpatterns = [
     path('', MovieView.as_view()),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
     path('review/<int:pk>/', AddReviewView.as_view(), name='add_review'),
+    path('actor/<str:slug>/', ActorView.as_view(), name='actor_detail'),
 ]
