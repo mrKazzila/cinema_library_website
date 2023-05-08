dbe06b05bf02bf1d1c54e61b2f50e2c573e12d42
Kazzila <mrkazzila@gmail.com>
2023-05-08 23:56:49 +0300

    feat: add new url path for subscription

--- app/config/urls.py
+++ app/config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
+    path('subscription/', include('subscription.urls')),
     path('', include('cinemalib.urls')),
 ]
 
