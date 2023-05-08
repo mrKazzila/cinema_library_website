26e4c20a5675e2cc47bd6121dcb5270a7b953b04
Kazzila <mrkazzila@gmail.com>
2023-05-08 21:03:00 +0300

    feat: add new url path for rating

--- app/cinemalib/urls.py
+++ app/cinemalib/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ActorView, AddReviewView, FilterMovieView, MovieDetailView, MovieView
+from .views import ActorView, AddReviewView, AddStarRatingView, FilterMovieView, MovieDetailView, MovieView
 
 urlpatterns = [
     path('', MovieView.as_view()),
     path('filter/', FilterMovieView.as_view(), name='filter'),
+    path('add_rating/', AddStarRatingView.as_view(), name='add_rating'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
     path('review/<int:pk>/', AddReviewView.as_view(), name='add_review'),
     path('actor/<str:slug>/', ActorView.as_view(), name='actor_detail'),
