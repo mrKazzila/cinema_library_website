5b63e32f4818f3f4f1182f272a2c23f6cc1845f2
Kazzila <mrkazzila@gmail.com>
2023-05-08 22:34:01 +0300

    feat: add recaptcha settings

--- app/config/settings/__init__.py
+++ app/config/settings/__init__.py
@@ -2,3 +2,9 @@ from .common import *  # noqa: F403, F401
 from .database import DATABASES  # noqa: F401
 from .logging import LOGGING  # noqa: F401
 from .ckeditor import CKEDITOR_CONFIGS, CKEDITOR_UPLOAD_PATH  # noqa: F401
+from .recaptcha import (  # noqa: F403, F401
+    RECAPTCHA_DEFAULT_ACTION,
+    RECAPTCHA_PRIVATE_KEY,
+    RECAPTCHA_PUBLIC_KEY,
+    RECAPTCHA_SCORE_THRESHOLD,
+)
