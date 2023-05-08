4e9f60a3c67d0504b2760cd39d92e1821ee7a326
Kazzila <mrkazzila@gmail.com>
2023-05-08 18:12:36 +0300

    feat: add new url path for filter

--- app/cinemalib/urls.py
+++ app/cinemalib/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ActorView, AddReviewView, MovieDetailView, MovieView
+from .views import ActorView, AddReviewView, FilterMovieView, MovieDetailView, MovieView
 
 urlpatterns = [
     path('', MovieView.as_view()),
+    path('filter/', FilterMovieView.as_view(), name='filter'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
     path('review/<int:pk>/', AddReviewView.as_view(), name='add_review'),
     path('actor/<str:slug>/', ActorView.as_view(), name='actor_detail'),
