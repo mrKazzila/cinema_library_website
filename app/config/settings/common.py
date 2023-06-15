075d111adf29ac64aa750c1f27aee4a429444aca
mrKazzila <mrkazzila@gmail.com>
2023-06-15 20:09:29 +0300

    chore: remove blank lines

--- app/config/settings/common.py
+++ app/config/settings/common.py
@@ -72,28 +72,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = (BASE_DIR / '../static',)
-else:
-    STATIC_ROOT = BASE_DIR / '../static'  # for prod
+STATICFILES_DIRS = (BASE_DIR / '../static',)
+STATIC_ROOT = 'static'
 
 # Media files
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / '../media'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
