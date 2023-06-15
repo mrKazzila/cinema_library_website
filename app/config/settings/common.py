6bcbfc37423dac420ec74c114fdb1981ebda71ef
mrKazzila <mrkazzila@gmail.com>
2023-06-15 20:12:31 +0300

    fix: fix SystemCheckError

--- app/config/settings/common.py
+++ app/config/settings/common.py
@@ -77,17 +77,14 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+# Static files
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '../../static'
 STATICFILES_DIRS = (BASE_DIR / '../static',)
-STATIC_ROOT = 'static'
 
 # Media files
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = BASE_DIR / '../../media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
